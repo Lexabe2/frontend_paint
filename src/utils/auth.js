3e71d8d79a6096b3bd1d51774b5e516f8ca07fff
Alekseu <natash79@inbox.ru>
2025-05-27 12:34:55 +0300

    Изменил axios

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
+import api from '../api/axios';
 
 export async function checkAuth() {
   const access = localStorage.getItem('access_token');
   const refresh = localStorage.getItem('refresh_token');
 
   try {
-    await axios.post('http://127.0.0.1:8000/api/token/verify/', { token: access });
+    await api.post('/token/verify/', { token: access });
     return true;
   } catch (err) {
     // access истёк
     if (refresh) {
       try {
-        const res = await axios.post('http://127.0.0.1:8000/api/token/refresh/', { refresh });
+        const res = await api.post('/token/refresh/', { refresh });
         localStorage.setItem('access_token', res.data.access);
         return true;
       } catch {
