98b19bf9cc426a6f1781dd59b06161f4b58089f2
aleksej <natash79@inbox.ru>
2025-06-03 09:52:42 +0300

    Добавил проверку авторизации каждый час

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
+// Добавляем Authorization заголовок ко всем запросам
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default api;
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import api from '../api/axios'; // axios instance с токеном
 
 export default function Layout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await api.get('check-auth/');
+        console.log("Тест")
+        // если статус 200 — всё ок
+      } catch (err) {
+        console.warn('Сессия истекла или недействительна');
+        localStorage.removeItem('access_token');
+        navigate('/login');
+      }
+    };
+
+    checkAuth(); // при монтировании
+
+    const interval = setInterval(checkAuth, 60 * 60 * 1000); // каждые 60 минут
+    
+
+    return () => clearInterval(interval); // очистка при размонтировании
+  }, [navigate]);
+
   return (
     <>
       <Header />
