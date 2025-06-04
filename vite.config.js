2326371b9dd84eef04df8594aab7c484060be5ff
aleksej <natash79@inbox.ru>
2025-06-04 09:21:29 +0300

    Добавил приложение

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['apple-touch-icon.png'],
+      devOptions: {
+        enabled: true // 👈 ДОБАВЬ ЭТО
+      },
+      manifest: {
+        name: 'Покрасочная',
+        short_name: 'App',
+        description: 'Покрасочная',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pweb-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
+  server: {
+    host: true, // чтобы можно было открыть с телефона
+  },
 })
